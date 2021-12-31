8e75848f21206050df8ff36d0d84d6959da3c592
OpenGG <liy099@gmail.com>
2021-12-31 20:08:47 +0800

    fix: hmr

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
+const isCodeSandbox = process.cwd() === '/sandbox'
+
+const server = isCodeSandbox && {
+  hmr: {
+    port: 443
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   hmr: {
-  //     port: 443
-  //   }
-  // },
+  server,
   build: {
     minify: false,
   },
