568e4c9fe75bf1197331b7ab9882d18d0198c41d
OpenGG <liy099@gmail.com>
2022-01-05 20:09:32 +0800

    feat: Move to stackblitz

--- 1.sh
+++
@@ -1,3 +0,0 @@
-rm vite.config.ts.out
-viteConfig=`ls vite.config.*`
-sed -e 's|defineConfig({|defineConfig({\n  server: process.cwd() === "/sandbox" \&\& {\n    hmr: {\n      port: 443\n    }\n  },|' $viteConfig > $viteConfig.out
